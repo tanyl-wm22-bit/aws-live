82a36978d2dc81927c8313e0a19c6e84ce29926a
TanYouLe <tanyl-wm22@student.tarc.edu.my>
2025-08-19 15:37:34 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "employee.c075fpqbjptl.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "tanyoule-employee"
 customregion = "us-east-1"
 
